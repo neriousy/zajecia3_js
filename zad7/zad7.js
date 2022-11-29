421cfd4f835287b0dd987dc4df783641bc3345ef
neriousy <filipori7@interia.pl>
2022-11-29 10:30:06 +0100

    zadanie 7

---
+++ zad7/zad7.js
@@ -0,0 +1,25 @@
+function isHappyNumber(x){
+    let value = parseInt(x);
+    let newValue = 0;
+    
+    for(i = 0; i < 10; i++){
+        if(value == 1){
+            return true;
+        }
+        
+        while(value >= 1){
+            if(value >= 10){
+                newValue += Math.pow((value%10), 2);    
+            }else{
+                newValue += Math.pow(value, 2);
+            }
+            value = parseInt(value / 10);
+            
+        }
+        value = newValue;
+        newValue = 0;
+    }
+
+    return false;
+     
+}
