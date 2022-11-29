114fe0d9f74ddc6db4d2dce1f2d2fbd2d17a178a
neriousy <filipori7@interia.pl>
2022-11-29 10:03:32 +0100

    zad6 better

--- zad6/zad6.js
+++ zad6/zad6.js
@@ -1,11 +1,7 @@
 function palindrom(a){
-    a = a.toLowerCase();
-    for(i = 0; i < a.length / 2; i++){
+    let value = a.toLowerCase();
 
-        if(a[i] != a[a.length - 1 - i]){
-            return false;
-        }
-    }
+    b = a.split('').reverse().join('');
 
-    return true;
+    return a == b;
 }
