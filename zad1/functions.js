5c29dbf3477d815ee9b63e141a45625516812a25
neriousy <filipori7@interia.pl>
2022-11-29 08:45:31 +0100

    zad1

---
+++ zad1/functions.js
@@ -0,0 +1,13 @@
+function isOdd(x){
+    if(x%2 == 0){
+        return false;
+    }
+    return true;
+}
+
+function isEven(x){
+    if(x%2 == 0){
+        return true;
+    }
+    return false;
+}
